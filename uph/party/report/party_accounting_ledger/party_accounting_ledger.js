228ffb0098077bef850061f5ed38a9c98cc06a8c
Abdo Ruzaqi <ruzaqi@gmail.com>
2025-04-28 21:25:48 +0300

    Dailly Commits

--- uph/party/report/party_accounting_ledger/party_accounting_ledger.js
+++ uph/party/report/party_accounting_ledger/party_accounting_ledger.js
@@ -46,6 +46,7 @@ frappe.query_reports["Party Accounting Ledger"] = {
 			get_data: function (txt) {
 				return frappe.db.get_link_options("Account", txt, {
 					company: frappe.query_report.get_filter_value("company"),
+          account_type:["in",["Receivable","Payable"]],
 				});
 			},
 		},
