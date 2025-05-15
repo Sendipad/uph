030b8cd031a9f4b8cf2999de40544b5224527d67
Abdo Ruzaqi <ruzaqi@gmail.com>
2025-05-16 02:49:24 +0300

    Create __init__.py

---
+++ uph/tests/__init__.py
@@ -0,0 +1,7 @@
+def before_tests():
+    print(">>> [test_app] before_tests called")
+    # Example: setup dummy records or patch values
+
+def after_tests():
+    print(">>> [test_app] after_tests called")
+    # Example: clean up temp data
