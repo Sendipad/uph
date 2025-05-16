bc1804849f9c07b37b15bef2ffdabeefba1da35d
Abdo Ruzaqi <ruzaqi@gmail.com>
2025-05-16 03:04:04 +0300

    Update test_party_controller.py

--- uph/party/controllers/test/test_party_controller.py
+++ uph/party/controllers/test/test_party_controller.py
@@ -7,8 +7,6 @@ from uph.party.doctype.party_master.party_master import create_party_from_party_
 class TestPartyController(AccountsTestMixin, FrappeTestCase):
     def setUp(self):
         self.create_company()
-        self.create_usd_receivable_account()
-        self.create_usd_payable_account()
         self.create_item()
 
     def tearDown(self):
