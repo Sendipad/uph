9fbe250d90e9d89602664c93c956cf3d759db10d
Sendipad <sendipad@example.com>
2026-02-05 21:07:37 +0300

    chore(release): bump version to v2.6.0

--- uph/__init__.py
+++ uph/__init__.py
@@ -5,7 +5,7 @@ from uph.party.controllers.queries import (
 )
 from uph.party.controllers.cache_utils import SmartCache
 
-__version__ = "2.5.5"
+__version__ = "2.6.0"
 
 
 # ----------------------------------------------------------------------------
