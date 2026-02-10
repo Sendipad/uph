b9ade40d80f418e5805fd479b2f4b7378274e115
Abo Arkan Ruzaqi <ruzaqi@gmail.com>
2026-02-10 04:01:51 +0300

    Handle QUnit results rendered in iframe

--- cypress/e2e/qunit.cy.js
+++ cypress/e2e/qunit.cy.js
@@ -9,11 +9,30 @@ describe("UPH QUnit Tests", () => {
 		});
 	};
 
+	const assertQUnitPassed = (root) => {
+		cy.wrap(root)
+			.find("#qunit-testresult", { timeout: 180000 })
+			.should("contain", "completed");
+		cy.wrap(root).find("#qunit-testresult .failed").should("contain", "0");
+	};
+
 	it("runs the QUnit suite for the UPH app", () => {
 		login();
-
 		cy.visit("/app/tests?app=uph");
-		cy.get("#qunit-testresult", { timeout: 120000 }).should("contain", "completed");
-		cy.get("#qunit-testresult .failed").should("contain", "0");
+
+		cy.get("body", { timeout: 60000 }).then(($body) => {
+			if ($body.find("#qunit-testresult").length) {
+				assertQUnitPassed($body);
+				return;
+			}
+
+			cy.get("iframe", { timeout: 60000 })
+				.first()
+				its("0.contentDocument.body")
+				.should("not.be.empty")
+				.then((iframeBody) => {
+					assertQUnitPassed(iframeBody);
+				});
+		});
 	});
 });
